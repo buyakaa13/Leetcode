b6c6393e0914f3410d9c3d5aecfaa8bffba0b9f5
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-02-19 19:35:44 -0600

    Time: 1 ms (71.21%) | Memory: 42.1 MB (88.16%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        String max = str1.length() >= str2.length() ? str1 : str2;
+        String min = str1.length() < str2.length() ? str1 : str2;
+        if(!max.startsWith(min))
+            return "";
+        while(!min.isEmpty()){
+            if(max.contains(min)){
+                String temp = min;
+                min = max.replace(min, "");
+                max = temp;
+            }
+            else
+                return "";
+        }
+        return max;
+    }
+}
