4a5d01a86c2caf17265f7adfab72d3982bc33f7a
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-02-22 10:04:58 -0600

    Time: 7 ms (42.49%) | Memory: 45 MB (36.26%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode recoverFromPreorder(String traversal) {
+        Stack<TreeNode> stack = new Stack<>();
+        int index = 0;
+
+        while (index < traversal.length()) {
+            // Count the number of dashes
+            int depth = 0;
+            while (
+                index < traversal.length() && traversal.charAt(index) == '-'
+            ) {
+                depth++;
+                index++;
+            }
+
+            // Extract the node value
+            int value = 0;
+            while (
+                index < traversal.length() &&
+                Character.isDigit(traversal.charAt(index))
+            ) {
+                value = value * 10 + (traversal.charAt(index) - '0');
+                index++;
+            }
+
+            // Create the current node
+            TreeNode node = new TreeNode(value);
+
+            // Adjust the stack to the correct depth
+            while (stack.size() > depth) {
+                stack.pop();
+            }
+
+            // Attach the node to the parent
+            if (!stack.empty()) {
+                if (stack.peek().left == null) {
+                    stack.peek().left = node;
+                } else {
+                    stack.peek().right = node;
+                }
+            }
+
+            // Push the current node onto the stack
+            stack.push(node);
+        }
+
+        // The root is the first node in the stack
+        while (stack.size() > 1) {
+            stack.pop();
+        }
+
+        return stack.peek();
+    }
+}
