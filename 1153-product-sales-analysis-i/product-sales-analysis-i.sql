0c5236d0aa60d30de3586ebc728f1689e05b1e3d
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-03-08 16:26:55 -0600

    Time: 6837 ms (10.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+-- SELECT B.product_name, A.year, A.price FROM Sales A
+-- INNER JOIN Product B ON A.product_id = B.product_id
+
+SELECT A.product_name, B.year, B.price FROM Product A
+INNER JOIN Sales B ON A.product_id = B.product_id
