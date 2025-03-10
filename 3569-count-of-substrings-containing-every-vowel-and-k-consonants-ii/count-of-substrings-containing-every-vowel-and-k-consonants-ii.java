c10c3fd0f8fdeff4437843b5bd428e6903f01c5f
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-03-10 15:02:58 -0500

    Time: 53 ms (100.00%) | Memory: 46.6 MB (36.75%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,37 +1,46 @@
 class Solution {
     public long countOfSubstrings(String word, int k) {
-        return atLeastK(word, k) - atLeastK(word, k+1);
-    }
+        int[][] frequencies = new int[2][128];
+        frequencies[0]['a'] = 1;
+        frequencies[0]['e'] = 1;
+        frequencies[0]['i'] = 1;
+        frequencies[0]['o'] = 1;
+        frequencies[0]['u'] = 1;
 
-    private long atLeastK(String word, int k){
-        long numValidSubstrings = 0;
-        int start = 0, end = 0;
-        HashMap<Character, Integer> vowelCount = new HashMap<>();
-        int consonantCount = 0;
-        while(end < word.length()){
-            char letter = word.charAt(end);
-            if(isVowel(letter))
-                vowelCount.put(letter, vowelCount.getOrDefault(letter, 0) + 1);
-            else
-                consonantCount++;
+        long response = 0;
+        int currentK = 0, vowels = 0, extraLeft = 0;
 
-            while(vowelCount.size() == 5 && consonantCount >= k){
-                numValidSubstrings += word.length() - end;
-                char startLetter = word.charAt(start);
-                if(isVowel(startLetter)){
-                    vowelCount.put(startLetter, vowelCount.get(startLetter)-1);
-                    if(vowelCount.get(startLetter) == 0)
-                        vowelCount.remove(startLetter);
-                }
-                else consonantCount--;
-                start++;
+        for (int right = 0, left = 0; right < word.length(); right++) {
+            char rightChar = word.charAt(right);
+
+            if (frequencies[0][rightChar] == 1) {
+                if (++frequencies[1][rightChar] == 1) vowels++;
+            } else {
+                currentK++;
             }
-            end++;
-        }
-        return numValidSubstrings;
-    }
 
-    private boolean isVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+            while (currentK > k) {
+                char leftChar = word.charAt(left);
+                if (frequencies[0][leftChar] == 1) {
+                    if (--frequencies[1][leftChar] == 0) vowels--;
+                } else {
+                    currentK--;
+                }
+                left++;
+                extraLeft = 0;
+            }
+
+            while (vowels == 5 && currentK == k && left < right && frequencies[0][word.charAt(left)] == 1 && frequencies[1][word.charAt(left)] > 1) {
+                extraLeft++;
+                frequencies[1][word.charAt(left)]--;
+                left++;
+            }
+
+            if (currentK == k && vowels == 5) {
+                response += (1 + extraLeft);
+            }
+        }
+
+        return response;
     }
 }
