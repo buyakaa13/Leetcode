8c5173882a37670f03a77784cd5e68b8b6e00db4
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-03-28 14:41:59 -0500

    Time: 2 ms (98.84%) | Memory: 42.2 MB (81.45%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -3,10 +3,11 @@ class Solution {
         if(cost.length == 1) return cost[0];
         Arrays.sort(cost);
         int sum = 0;
-        for(int i=0; i<cost.length; i++)
-            sum += cost[i];
-        for(int i=cost.length-3; i>=0; i-=3)
-            sum-=cost[i];
+        for(int i=cost.length-1; i>=0; i-=3){
+            sum+=cost[i];
+            if(i-1 >= 0)
+                sum+=cost[i-1];
+        }
         return sum;
     }
 }
