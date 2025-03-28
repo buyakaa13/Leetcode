cf58069e934d64dfe3356f245fa6df438c05f2d2
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-03-27 20:19:52 -0500

    Time: 367 ms (99.82%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,3 @@
 SELECT A.id FROM Weather A
 INNER JOIN Weather B ON A.recordDate = ISNULL(DATEADD(day, 1, B.recordDate), B.recordDate)
-WHERE A.temperature - B.temperature > 0
+WHERE A.temperature > B.temperature
