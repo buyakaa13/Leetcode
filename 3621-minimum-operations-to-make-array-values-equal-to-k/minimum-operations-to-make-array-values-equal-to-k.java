ee23b11e1eda6d72536a2e7cb3a22da32dabbc04
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-09 14:02:21 -0700

    Time: 5 ms (15.75%) | Memory: 44.3 MB (89.04%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        if(nums.length == 1 && nums[0] == k)
+            return 0;
+        Arrays.sort(nums);
+        TreeSet<Integer> map = new TreeSet<>();
+        int count = 0;
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i] < k)
+                return -1;
+            else if(!map.contains(nums[i])){
+                map.add(nums[i]);
+                if(nums[i] > k)
+                    count++;
+            }
+        }
+        return map.size() > 0 ? count : -1;
+    }
+}
