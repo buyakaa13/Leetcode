f2cb21ec962d9b6a5f4c2b7c260f9239b3d32250
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-14 15:20:53 -0700

    Time: 16 ms (13.59%) | Memory: 42.2 MB (27.85%) - LeetSync

--- 2122-count-special-quadruplets/count-special-quadruplets.java
+++ 2122-count-special-quadruplets/count-special-quadruplets.java
@@ -1,10 +1,10 @@
 class Solution {
     public int countQuadruplets(int[] nums) {
         int len = nums.length, count = 0; 
-        for(int a=0; a<=len-4; a++){
-            for(int b=a+1; b<=len-3; b++){
-                for(int c=b+1; c<=len-2; c++){
-                    for(int d=c; d<=len-1; d++){
+        for(int a=0; a<len-3; a++){
+            for(int b=a+1; b<len-2; b++){
+                for(int c=b+1; c<len-1; c++){
+                    for(int d=c; d<len; d++){
                         if(nums[a] + nums[b] + nums[c] == nums[d])
                             count++;
                     }
