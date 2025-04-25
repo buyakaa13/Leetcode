fdb603e1056ba0f8d618d279e3255553d9a7275b
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-24 18:01:46 -0700

    Time: 736 ms (5.02%) | Memory: 46.5 MB (8.51%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
+        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        int steps = 0;
+        while(!queue.isEmpty()){
+            steps++;
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                int[] current = queue.poll();
+                for(int[] dir: directions){
+                    int row = dir[0] + current[0];
+                    int col = dir[1] + current[1];
+                    System.out.println("row: " + row + " col: " + col + " step: " + steps);
+                    if(row < 0 || row >= maze.length || col < 0 || col >= maze[0].length) continue;
+                    else if(maze[row][col] == '+') continue;
+                    else if(row == 0 || row == maze.length-1 || col == 0 || col == maze[0].length-1) return steps;
+                   
+                    maze[row][col] = '+';
+                    queue.offer(new int[]{row, col});
+                }
+            }
+        }
+        return -1;
+    }
+}
