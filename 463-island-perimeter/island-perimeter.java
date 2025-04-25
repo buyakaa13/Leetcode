86b02a72e591186dd7ffb71fd293f35eb43147f3
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-25 15:17:39 -0700

    Time: 4 ms (99.77%) | Memory: 45.1 MB (94.88%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,20 +1,16 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
         int perimeter = 0;
-        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0, -1}};
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 0) continue;
-                for(int[] dir: directions){
-                    int row = i + dir[0];
-                    int col = j + dir[1];
-                    if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) perimeter++;
-                    // if(row == 0 || row == grid.length-1) 
-                    //     perimeter++;
-                    // if(col == 0 || col == grid[0].length-1) 
-                    //     perimeter++;
-                    else if(grid[row][col] == 0)
-                        perimeter++;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == 1){
+                    perimeter += 4;
+                    if(j < cols-1 && grid[i][j+1] == 1)
+                        perimeter -= 2;
+                    if(i < rows-1 && grid[i+1][j] == 1)
+                        perimeter -= 2;
                 }
             }
         }
