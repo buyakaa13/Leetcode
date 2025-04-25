4e1a3032a5a667879cdb2ae3a1f8f5cbc07204a2
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-25 15:07:33 -0700

    Time: 6 ms (34.16%) | Memory: 45.4 MB (87.85%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,35 +1,23 @@
 class Solution {
-    static int[] dRow = {-1, 0, 1, 0};
-    static int[] dCol = {0, 1, 0, -1};
-
-    static boolean isValid(int row, int col, int rowCount, int colCount){
-        if(row < 0 || col < 0 || row >= rowCount || col >= colCount) return false;
-        return true;
-    }
-
     public int islandPerimeter(int[][] grid) {
-        if(grid.length == 0) return 0;
-        int rowCount = grid.length;
-        int colCount = grid[0].length;
-        int p = 0;
+        int perimeter = 0;
+        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0, -1}};
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 1)
-                    p += BFS(i, j, rowCount, colCount, grid);
+                if(grid[i][j] == 0) continue;
+                for(int[] dir: directions){
+                    int row = i + dir[0];
+                    int col = j + dir[1];
+                    if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) perimeter++;
+                    // if(row == 0 || row == grid.length-1) 
+                    //     perimeter++;
+                    // if(col == 0 || col == grid[0].length-1) 
+                    //     perimeter++;
+                    else if(grid[row][col] == 0)
+                        perimeter++;
+                }
             }
         }
-        return p;
-    }
-
-    public int BFS(int row, int col, int rowCount, int colCount, int[][] grid){
-        int count = 4;
-        for(int i=0; i<4; i++){
-            int adjX = row + dRow[i];
-            int adjY = col + dCol[i];
-            if(isValid(adjX, adjY, rowCount, colCount) && grid[adjX][adjY] == 1){
-                count--;
-            }
-        }
-        return count;
+        return perimeter;
     }
 }
