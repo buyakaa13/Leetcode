dee76edf9934b76a2d5fa1ff307af414a9c1ba77
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-26 12:13:11 -0700

    Time: 4 ms (48.67%) | Memory: 50 MB (30.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,55 +1,23 @@
 class Solution {
+    int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
     public int numIslands(char[][] grid) {
-        if(grid.length == 0) return 0;
-        int rowCount = grid.length;
-        int colCount = grid[0].length;
-        int landCount = 0;
-        boolean visited[][] = new boolean[rowCount][colCount];
-        for(int i=0; i<rowCount; i++){
-            for(int j=0; j<colCount; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
-                    BFS(i, j, grid, visited, rowCount, colCount);
-                    landCount++;
+        int islands = 0;
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
+                if(grid[r][c] == '1'){
+                    dfs(r, c, grid);
+                    islands++;
                 }
             }
         }
-        return landCount;
+        return islands;
     }
 
-    public class Pair{
-        int x, y;
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    static int[] dRow = new int[]{-1, 0, 1, 0};
-    static int[] dCol = new int[]{0, 1, 0, -1};
-
-    static boolean isValid(int row, int col, int rowCount, int colCount, boolean[][] visited){
-        if(row < 0 || col < 0 || row >= rowCount || col >= colCount) return false;
-        if(visited[row][col]) return false;
-        return true;
-    }
-
-    public void BFS(int row, int col, char[][] grid, boolean[][] visited, int rowCount, int colCount){
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(row, col));
-        visited[row][col] = true;
-
-        while(!queue.isEmpty()){
-            Pair current = queue.poll();
-            int x = current.x;
-            int y = current.y;
-            for(int i=0; i<4; i++){
-                int adjX = x + dRow[i];
-                int adjY = y + dCol[i];
-                if(isValid(adjX, adjY, rowCount, colCount, visited) && grid[adjX][adjY] == '1'){
-                    queue.add(new Pair(adjX, adjY));
-                    visited[adjX][adjY] = true;
-                }
-            }
+    public void dfs(int row, int col, char[][] grid){
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') return;
+        grid[row][col] = '0';
+        for(int[] dir: directions){
+            dfs(row+dir[0], col+dir[1], grid);
         }
     }
 }
