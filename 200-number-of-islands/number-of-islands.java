947699334a813d22e7013b1d48a39318af91dda8
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-04-26 12:47:36 -0700

    Time: 4 ms (48.60%) | Memory: 52.4 MB (14.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,11 @@
 class Solution {
-    int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
     public int numIslands(char[][] grid) {
         int islands = 0;
-        for(int r=0; r<grid.length; r++){
-            for(int c=0; c<grid[0].length; c++){
+        int rowCount = grid.length, colCount = grid[0].length;
+        for(int r=0; r<rowCount; r++){
+            for(int c=0; c<colCount; c++){
                 if(grid[r][c] == '1'){
-                    dfs(r, c, grid);
+                    bfs(r, c, rowCount, colCount, grid);
                     islands++;
                 }
             }
@@ -13,11 +13,33 @@ class Solution {
         return islands;
     }
 
-    public void dfs(int row, int col, char[][] grid){
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') return;
+    public void bfs(int row, int col, int rowCount, int colCount, char[][] grid){
+        Queue<int[]> queue = new LinkedList();
+        queue.offer(new int[]{row, col});
         grid[row][col] = '0';
-        for(int[] dir: directions){
-            dfs(row+dir[0], col+dir[1], grid);
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int r = current[0], c = current[1];
+            if(r+1 < rowCount && grid[r+1][c] == '1'){
+                queue.offer(new int[]{r+1, c});
+                grid[r+1][c] = '0';
+            }
+
+            if(c+1 < colCount && grid[r][c+1] == '1'){
+                queue.offer(new int[]{r, c+1});
+                grid[r][c+1] = '0';
+            }
+
+            if(r-1 >= 0 && grid[r-1][c] == '1'){
+                queue.offer(new int[]{r-1, c});
+                grid[r-1][c] = '0';
+            }
+
+            if(c-1 >= 0 && grid[r][c-1] == '1'){
+                queue.offer(new int[]{r, c-1});
+                grid[r][c-1] = '0';
+            }
         }
     }
 }
