c078f36594913f2118e02113efad22d4ac1e9041
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-09 17:50:14 -0700

    Time: 10 ms (37.26%) | Memory: 42.7 MB (15.77%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,25 +1,10 @@
 class Solution {
-    String[][] hour = {{"0"},
-            {"1", "2", "4", "8"},
-            {"3", "5", "6", "9", "10"},
-            {"7", "11"}};
-    String[][] minute = {{"00"},
-            {"01", "02", "04", "08", "16", "32"},
-            {"03", "05", "06", "09", "10", "12", "17", "18", "20", "24", "33", "34", "36", "40", "48"},
-            {"07", "11", "13", "14", "19", "21", "22", "25", "26", "28", "35", "37", "38", "41", "42", "44", "49", "50", "52", "56"},
-            {"15", "23", "27", "29", "30", "39", "43", "45", "46", "51", "53", "54", "57", "58"},
-            {"31", "47", "55", "59"}};
-    public List<String> readBinaryWatch(int turnedOn) {
-        List<String> result = new ArrayList<>();
-        for(int i=0; i<=3 && i<= turnedOn; i++){
-            if(turnedOn-i <= 5){
-                for(String str1: hour[i]){
-                    for(String str2: minute[turnedOn-i]){
-                        result.add(str1 + ":" + str2);
-                    }
-                }
-            }
-        }
-        return result;
+    public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+        for (int h=0; h<12; h++)
+            for (int m=0; m<60; m++)
+                if (Integer.bitCount(h * 64 + m) == num)
+                    times.add(String.format("%d:%02d", h, m));
+        return times; 
     }
 }
