0c1112ca5a7756aa36cc54d4962b3d4bcba981c7
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-10 11:28:43 -0700

    Time: 12 ms (2.09%) | Memory: 44.9 MB (5.47%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        HashSet<List<Integer>> result = new HashSet<>();
         boolean[] used = new boolean[nums.length];
         backtrack(nums, used, 0, result);
-        return result;
+        return new ArrayList(result);
     }
 
-    public void backtrack(int[] nums, boolean[] used, int start, List<List<Integer>> result){
+    public void backtrack(int[] nums, boolean[] used, int start, HashSet<List<Integer>> result){
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i<nums.length; i++){
+        for(int i=0; i<nums.length; i++){
             if(used[i])
                 list.add(nums[i]);
         }
-
+        Collections.sort(list);
         result.add(list);
 
         for(int i=start; i<nums.length; i++){
             if(!used[i]){
                 used[i] = true;
-                backtrack(nums, used, i+1, result);
+                backtrack(nums, used, start+1, result);
                 used[i] = false;
             }
         }
