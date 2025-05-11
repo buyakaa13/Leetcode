6cd840652ad19875f1ab86dd440bd528792fb54c
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-10 19:19:01 -0700

    Time: 1 ms (87.01%) | Memory: 41.4 MB (28.86%) - LeetSync

--- 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
+++ 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
@@ -1,17 +1,11 @@
 class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        int count = numOnes + numZeros + numNegOnes;
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<numOnes; i++)
-            list.add(1);
-        for(int i=0; i<numZeros; i++)
-            list.add(0);  
-        for(int i=0; i<numNegOnes; i++)
-            list.add(-1);  
-        int sum = 0;
-        for(int i=0; i<k; i++){
-            sum += list.get(i);
-        }
-        return sum;
+        if(k <= numOnes) return k;
+        else if(k <= numZeros+numOnes) return numOnes;
+        else if(k <= numZeros+numOnes + numNegOnes) {
+            int l = k - numOnes-numZeros;
+            return numOnes - l;
+        } 
+        else return -1;
     }
 }
