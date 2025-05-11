da9cc0e0ba14fe99ad0e951d7b65e2a533a98080
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-11 07:38:24 -0700

    Time: 6 ms (24.49%) | Memory: 52.4 MB (14.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,24 +1,38 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int islands = 0;
-        int rowCount = grid.length, colCount = grid[0].length;
-        for(int r=0; r<rowCount; r++){
-            for(int c=0; c<colCount; c++){
-                if(grid[r][c] == '1'){
-                    bfs(r, c, rowCount, colCount, grid);
-                    islands++;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    bfs(grid, visited, i, j);
+                    count++;
                 }
             }
         }
-        return islands;
+        return count;
     }
 
-    public void bfs(int row, int col, int rowCount, int colCount, char[][] grid){
-        if(row < 0 || row >= rowCount || col < 0 || col >= colCount || grid[row][col] == '0') return;
-        grid[row][col] = '0';
-        bfs(row+1, col, rowCount, colCount, grid);
-        bfs(row-1, col, rowCount, colCount, grid);
-        bfs(row, col+1, rowCount, colCount, grid);
-        bfs(row, col-1, rowCount, colCount, grid);
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public void bfs(char[][] grid, boolean[][] visited, int row, int col){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{row, col});
+        visited[row][col] = true;
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int r = current[0];
+            int c = current[1];
+
+            for(int i=0; i<directions.length; i++){
+                int rowNeighbor = r+directions[i][0];
+                int colNeighbor = c+directions[i][1];
+                if(rowNeighbor >= 0 && rowNeighbor < grid.length && colNeighbor >= 0 && colNeighbor < grid[0].length && !visited[rowNeighbor][colNeighbor] && grid[rowNeighbor][colNeighbor] == '1'){
+                    queue.add(new int[]{rowNeighbor, colNeighbor});
+                    visited[rowNeighbor][colNeighbor] = true;
+                }
+            }
+        }
     }
 }
