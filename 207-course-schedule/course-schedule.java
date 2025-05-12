7bbc375ac177dc2d19a00a458efab3c94b4126e2
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-11 19:59:10 -0700

    Time: 7 ms (49.05%) | Memory: 45.1 MB (83.19%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adjList = new ArrayList<>();
         int[] inDegree = new int[numCourses];
-        
-        for (int i = 0; i < numCourses; i++)
+        for(int i=0; i<numCourses; i++)
             adjList.add(new ArrayList<>());
-        
-        for (int[] prereq : prerequisites) {
-            int course = prereq[0]; 
-            int prerequisite = prereq[1]; 
-            adjList.get(prerequisite).add(course);
+
+        for(int i=0; i<prerequisites.length; i++){
+            int course = prerequisites[i][0];
+            int prerequisite = prerequisites[i][1];
             inDegree[course]++;
+            adjList.get(prerequisite).add(course);
         }
 
         Queue<Integer> queue = new LinkedList<>();
@@ -20,15 +19,13 @@ class Solution {
         }
 
         List<Integer> result = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            int currentCourse = queue.poll();
-            result.add(currentCourse);
-
-            for (int nextCourse : adjList.get(currentCourse)) {
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            result.add(current);
+            for(int nextCourse: adjList.get(current)){
                 inDegree[nextCourse]--;
-                if (inDegree[nextCourse] == 0) {
-                    queue.offer(nextCourse);
-                }
+                if(inDegree[nextCourse] == 0)
+                    queue.add(nextCourse);
             }
         }
         return result.size() == numCourses;
