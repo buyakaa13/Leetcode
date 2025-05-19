9f1fe884b30fe03d461c1ebc1d8e1546bb4a8436
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-19 10:13:15 -0700

    Time: 372 ms (63.22%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -2,4 +2,4 @@
 SELECT ISNULL(A.employee_id, B.employee_id) AS employee_id FROM Salaries A
 FULL OUTER JOIN Employees B ON A.employee_id = B.employee_id
 WHERE B.employee_id IS NULL OR A.salary IS NULL
-ORDER BY ISNULL(A.employee_id, B.employee_id)
+ORDER BY 1 ASC
