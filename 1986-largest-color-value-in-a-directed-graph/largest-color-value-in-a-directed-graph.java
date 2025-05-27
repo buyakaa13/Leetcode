56cbd443477763fbe6f5bc590db48c1e125b8a04
buyakaa13 <110402445+buyakaa13@users.noreply.github.com>
2025-05-27 08:18:08 -0700

    Time: 101 ms (38.11%) | Memory: 101.8 MB (89.94%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.java
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.java
@@ -4,7 +4,8 @@ class Solution {
         List<List<Integer>> graph = new ArrayList<>();
         int[] indegree = new int[n];
 
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < n; i++) 
+            graph.add(new ArrayList<>());
         for (int[] edge : edges) {
             graph.get(edge[0]).add(edge[1]);
             indegree[edge[1]]++;
@@ -20,24 +21,19 @@ class Solution {
 
         int visited = 0;
         int maxCount = 0;
-
         while (!queue.isEmpty()) {
             int node = queue.poll();
             visited++;
-
             for (int neighbor : graph.get(node)) {
                 for (int i = 0; i < 26; i++) {
                     int curr = count[node][i] + (colors.charAt(neighbor) - 'a' == i ? 1 : 0);
                     count[neighbor][i] = Math.max(count[neighbor][i], curr);
                 }
-
                 indegree[neighbor]--;
                 if (indegree[neighbor] == 0) queue.offer(neighbor);
             }
-
             maxCount = Math.max(maxCount, Arrays.stream(count[node]).max().getAsInt());
         }
-
         return visited == n ? maxCount : -1;
     }
 }
