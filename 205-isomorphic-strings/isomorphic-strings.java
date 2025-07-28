a9017e3c26e5ff7a7f87aeab4d643f4fa88cf171
Buyandelger Tsendsuren <110402445+buyakaa13@users.noreply.github.com>
2025-07-28 14:28:46 -0500

    Time: 35 ms (5.02%) | Memory: 45.6 MB (5.02%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        int idx1=0, idx2 = 0;
+        for(int i=0; i<s.length(); i++){
+            char ch1 = s.charAt(i);
+            map1.put(ch1, map1.getOrDefault(ch1, i+1));
+            str1.append(map1.get(ch1));
+            char ch2 = t.charAt(i);
+            map2.put(ch2, map2.getOrDefault(ch2, i+1));
+            str2.append(map2.get(ch2));
+        }
+        System.out.println("str1: " + str1.toString() + " str2: " + str2.toString());
+        return str1.toString().equals(str2.toString());
+    }
+}
