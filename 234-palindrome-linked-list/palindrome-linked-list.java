456711175b2a84f92a81f5939896a95ab5fc4e96
Buyandelger Tsendsuren <110402445+buyakaa13@users.noreply.github.com>
2025-07-29 15:02:06 -0500

    Time: 712 ms (6.37%) | Memory: 57 MB (86.30%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,39 +10,25 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
+        if(head == null || head.next == null) return true;
+        ListNode slower = head, faster = head;
+        Stack<Integer> stack = new Stack<>();
+        // stack.push(head.val);
+        while(faster != null && faster.next != null){
+            System.out.println("slower 1: " + slower.val);
+            stack.push(slower.val);
+            slower = slower.next;
+            faster = faster.next.next;
         }
-        ListNode newHead=reverseList(slow.next);
-        ListNode c1=head;
-        ListNode c2=newHead;
-        while(c2!=null)
-        {
-            if(c1.val!=c2.val)
-            {
-                reverseList(newHead);
-                return false;
-            }
-            c1=c1.next;
-            c2=c2.next;
-        }
-        reverseList(newHead);
-        return true;
-    }
 
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode nextTemp = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextTemp;
+        if(faster != null)
+            slower = slower.next;
+
+        while(slower != null){
+            if(slower.val != stack.pop())
+                return false;
+            slower = slower.next;
         }
-        return prev;
+        return true;
     }
 }
