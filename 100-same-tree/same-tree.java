5cbb4feb50a6ec12f871fcebaea080734454693b
Buyandelger Tsendsuren <110402445+buyakaa13@users.noreply.github.com>
2025-08-12 12:30:15 -0500

    Time: 0 ms (100.00%) | Memory: 41 MB (84.62%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,7 +15,42 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null || q == null) return p == q;
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if(p == null && q == null) return true;
+        else if(p == null || q == null) return false;
+        else if(p.val != q.val) return false;
+        Stack<TreeNode> stack1 = new Stack();
+        Stack<TreeNode> stack2 = new Stack();
+        stack1.push(p);
+        stack2.push(q);
+        while(!stack1.isEmpty() && !stack2.isEmpty()){
+            int size1 = stack1.size();
+            int size2 = stack2.size();
+            if(size1 != size2) return false;
+            while(size1 > 0){
+                TreeNode current1 = stack1.pop();
+                TreeNode current2 = stack2.pop();
+                if(current1.left != null && current2.left != null){
+                    if(current1.left.val != current2.left.val)
+                    return false;
+                    else{
+                        stack1.push(current1.left);
+                        stack2.push(current2.left);
+                    }
+                }
+                else if((current1.left != null && current2.left == null)|| (current1.left == null && current2.left != null)) return false;
+                
+                if(current1.right != null && current2.right != null){
+                    if(current1.right.val != current2.right.val)
+                        return false;
+                    else{
+                        stack1.push(current1.right);
+                        stack2.push(current2.right);
+                    }
+                }
+                else if((current1.right != null && current2.right == null)|| (current1.right == null && current2.right != null)) return false;
+                size1--;
+            }
+        }
+        return true;
     }
 }
