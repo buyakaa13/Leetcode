230f193976662b33ac238aa04936e55945a87f56
Buyandelger Tsendsuren <110402445+buyakaa13@users.noreply.github.com>
2026-02-18 16:46:50 -0600

    Time: 4 ms (53.31%) | Memory: 46.8 MB (92.85%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             map.put(nums[i], i);          
         }
@@ -12,6 +12,6 @@ class Solution {
             }
         }
 
-        return new int[]{-1, -1};
+        return new int[]{};
     }
 }
