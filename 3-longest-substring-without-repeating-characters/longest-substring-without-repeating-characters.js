89325cb55fc3b7666ac537cfccd7902b303d5424
Buyandelger Tsendsuren <110402445+buyakaa13@users.noreply.github.com>
2026-06-06 15:48:10 -0500

    Time: 6 ms (65.97%) | Memory: 58.7 MB (47.63%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,15 +5,16 @@
 var lengthOfLongestSubstring = function(s) {
     let l = 0;
     let max = 0;
-    let set = new Set();
+    const set = new Set();
 
     for(let r=0; r<s.length; r++){
         while(set.has(s[r])){
             set.delete(s[l]);
             l++;
         }
-        max = Math.max(max, (r-l)+1);
         set.add(s[r]);
+        max = Math.max(max, r-l+1);
     }
+
     return max;
 };
